827e7d9245ca523f667cf1706ae1b8aa254af44d
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-04-16 15:23:15 +0530

    UI and UX changes added

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,6 +1,6 @@
 class HealthChecksController < ApplicationController
   def index
     @api = Api.find(params[:api_id])
-    @health_checks = @api.health_checks.order(checked_at: :desc)
+    @health_checks = @api.health_checks.order(checked_at: :desc).limit(50)
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,4 +4,4 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
-endg
+end
